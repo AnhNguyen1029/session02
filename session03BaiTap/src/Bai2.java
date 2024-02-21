ae44e56f94e6efbe2232375dd9f159fbbb969245
AnhNguyen1029 <doanhnguyenvn1998@gmail.com>
2024-02-21 21:29:47 +0900

    add file

--- session03BaiTap/src/Bai2.java
+++ session03BaiTap/src/Bai2.java
@@ -7,12 +7,12 @@ public class Bai2 {
         Scanner scanner=new Scanner(System.in);
         int num;
         num = scanner.nextInt();
-        int[] number = new int[num];
-        int avg = 0;
+        float[] number = new float[num];
+        float avg = 0;
         System.out.println("hãy nhập từng giá trị cho mảng:");
         for (int i = 0; i < num; i++) {
             System.out.println("Giá trị thứ "+(i+1)+" của mảng:");
-            number[i] = scanner.nextInt();
+            number[i] = scanner.nextFloat();
             avg += number[i];
         }
         System.out.println("Trung Bình cộng của các phần tử trong mảng là : "+avg/num);
